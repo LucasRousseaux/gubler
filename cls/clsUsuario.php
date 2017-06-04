2ab219f497a8cedab044008b18ce153c5603de7c
Unknown <lucas.rousseaux@gmail.com>
2017-06-04 19:49:29 -0300

    namespaces

--- cls/clsUsuario.php
+++ cls/clsUsuario.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace GublerSpace
+namespace Gubler;
 
 
 abstract class Constante {
@@ -14,7 +14,7 @@ abstract class Constante {
 
 }
 
-namespace GublerSpace\UsuarioSpace
+namespace Gubler\Usr;
 
 
 class Usuario {
@@ -141,12 +141,12 @@ class Perfil extends Usuario {
 
 try {
 
-		$dsn = 'mysql:host=localhost;dbname='.Constante::SCHEMA_NAME.';charset=utf8mb4;port:3306';
+		$dsn = 'mysql:host=localhost;dbname='.\Gubler\Constante::SCHEMA_NAME.';charset=utf8mb4;port:3306';
 		$db_user = 'root';
 		$db_pass = '';
 
-		$db = new PDO($dsn, $db_user, $db_pass);
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$db = new \PDO($dsn, $db_user, $db_pass);
+		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		$usuario = new Usuario($db);
 
 
